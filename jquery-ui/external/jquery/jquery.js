407006cd8a6b59ede9f541ac5d8e9097b1f96b8d
sureshsonii <92368713+sureshsonii@users.noreply.github.com>
2021-10-12 15:48:49 +0530

    Update jquery.js

--- jquery-ui/external/jquery/jquery.js
+++ jquery-ui/external/jquery/jquery.js
@@ -50,17 +50,24 @@ var slice = deletedIds.slice;
 
 var concat = deletedIds.concat;
 
-var push = deletedIds.push;
-
-var indexOf = deletedIds.indexOf;
-
-var class2type = {};
+C
 
 var toString = class2type.toString;
 
 var hasOwn = class2type.hasOwnProperty;
 
 var support = {};
+	<div id="dialog-message" title="Download complete">
+  <p>
+    <span class="ui-icon ui-icon-circle-check" style="float:left; margin:0 7px 50px 0;"></span>
+    Your files have downloaded successfully into the My Downloads folder.
+  </p>
+  <p>
+    Currently using <b>36% of your storage space</b>.
+  </p>
+</div>
+ 
+<p>Sed vel diam id libero <a href="http://example.com">rutrum convallis</a>. Donec aliquet leo vel magna. Phasellus rhoncus faucibus ante. Etiam bibendum, enim faucibus aliquet rhoncus, arcu felis ultricies neque, sit amet auctor elit eros a lectus.</p>
 
 
 
